b6ea834b089e9865c3913f38bd3c0252ddd40ad9
Samo Mandič <samo.mandic@gmail.com>
2022-02-11 12:17:44 +0100

    Add routes for API

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import "github.com/go-restapi-service/config"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-restapi-service/config"
+	"github.com/go-restapi-service/routes"
+	"log"
+)
 
 func main() {
 	//Connect to db
 	config.Connect()
 
+	//Init routes
+	router := gin.Default()
+
+	//Route Handlers / Endpoints
+	routes.Routes(router)
+
+	log.Fatal(router.Run(":8080"))
 }
