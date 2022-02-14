01e79835c5f4ce86c9ddb05eb69989a55e70ac07
Samo Mandič <samo.mandic@gmail.com>
2022-02-14 22:13:10 +0100

    Add Dockerfile and Docker Compose, minor bug fix

--- config/db.go
+++ config/db.go
@@ -1,17 +1,23 @@
 package config
 
 import (
+	"context"
 	"github.com/go-pg/pg/v10"
 	"github.com/go-restapi-service/controllers"
 	"log"
 	"os"
+	"time"
 )
 
 func Connect() *pg.DB {
+	addr := "localhost:5432"
+	if os.Getenv("ENV") == "docker" {
+		addr = "host.docker.internal:5432"
+	}
 	opts := &pg.Options{
 		User:     "postgres",
 		Password: "postgres",
-		Addr:     "localhost:5432",
+		Addr:     addr,
 		Database: "postgres",
 	}
 
@@ -23,9 +29,20 @@ func Connect() *pg.DB {
 
 	log.Printf("Connected to db")
 
-	controllers.CreateGroupTable(db)
-	controllers.CreateUserTable(db)
-	controllers.InitializeDB(db)
+	//try 3 times, sometimes it takes longer for database to accept connections
+	for i := 0; i < 3; i++ {
+		if db.Ping(context.Background()) == nil {
+			controllers.CreateGroupTable(db)
+			controllers.CreateUserTable(db)
+			controllers.InitializeDB(db)
 
-	return db
+			return db
+		} else {
+			time.Sleep(time.Second * 3)
+		}
+	}
+
+	log.Printf("Connection failed: cannot ping db")
+	os.Exit(100)
+	return nil
 }
