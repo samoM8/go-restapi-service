e1a09892742bc18a875150d4d63188dc7f10aad3
Samo Mandič <samo.mandic@gmail.com>
2022-02-15 16:54:55 +0100

    Swagger docs and UI

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/go-openapi/runtime/middleware"
 	"github.com/go-restapi-service/controllers"
 	"net/http"
 )
@@ -22,6 +23,14 @@ func Routes(router *gin.Engine) {
 	router.POST("groups", controllers.CreateGroup)
 	router.PUT("groups/:groupId", controllers.EditGroup)
 	router.DELETE("groups/:groupId", controllers.DeleteGroup)
+
+	//Serve swagger.yaml file
+	router.GET("/swagger.yaml", gin.WrapH(http.FileServer(http.Dir("./"))))
+
+	//Swagger UI
+	opts := middleware.SwaggerUIOpts{SpecURL: "/swagger.yaml"}
+	sh := middleware.SwaggerUI(opts, nil)
+	router.GET("/docs", gin.WrapH(sh))
 }
 
 func welcome(c *gin.Context) {
